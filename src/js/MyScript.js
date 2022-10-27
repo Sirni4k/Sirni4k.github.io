ec8a7eba0103fc251e91d005777c5ec257a3084e
Sirni4k <101874451+Sirni4k@users.noreply.github.com>
2022-10-27 17:35:38 +0300

    Проверка

---
+++ src/js/MyScript.js
@@ -0,0 +1,46 @@
+"use strict"
+
+function getLocation(){
+    
+    function getLocationTwo(){
+        console.log("i am here");
+    }
+    
+    getLocationTwo();
+}
+
+getLocation();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
